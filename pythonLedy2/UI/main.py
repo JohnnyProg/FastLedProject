0e831719f7b99b71de71a7bd9d382b8aca792a00
JohnnyProg <jangryta9@gmail.com>
2022-02-09 06:17:50 +0100

    Created new Window interface

--- pythonLedy2/UI/main.py
+++ pythonLedy2/UI/main.py
@@ -9,14 +9,48 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.Opcja3.clicked.connect(lambda: FunctionsUI.name_of_button_clicked(self))
 
-        self.ui.Opcja1.clicked.connect(lambda: self.ui.Pages_Widget.setCurrentWidget(self.ui.page_1))
-        self.ui.Opcja2.clicked.connect(lambda: self.ui.Pages_Widget.setCurrentWidget(self.ui.page_3))
+        QtWidgets.QSizeGrip(self.ui.size_grip)
+
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.ui.button_minimize.clicked.connect(lambda: self.showMinimized())
+        self.ui.button_close.clicked.connect(lambda: self.close())
+        self.ui.button_maximize.clicked.connect(lambda: FunctionsUI.changeSizeOfWindow(self))
+
+        def moveWindow(e):
+            print("1")
+            if self.isMaximized() == False:
+                print("2")
+                try:
+                    if e.buttons() == QtCore.Qt.LeftButton:
+                        print("3")
+                        self.move(self.pos() + e.globalPos() - self.clickPosition)
+                        self.clickPosition = e.globalPos()
+                        e.accept()
+                except Exception as e:
+                    print(e)
+
+        self.ui.header.mouseMoveEvent = moveWindow
+        self.ui.button_minimize_menu.clicked.connect(lambda: FunctionsUI.slideLeftMenu(self))
+
+
+        # change of widgets
+        self.ui.button_item1.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page))
+        self.ui.button_item2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_2))
+        self.ui.button_item3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_3))
+        # self.ui.button_opcja3.clicked.connect(lambda: FunctionsUI.name_of_button_clicked(self))
+        #
+        # self.ui.button_opcja1.clicked.connect(lambda: self.ui.Pages_Widget.setCurrentWidget(self.ui.page_1))
+        # self.ui.button_opcja2.clicked.connect(lambda: self.ui.Pages_Widget.setCurrentWidget(self.ui.page_3))
 
 
         self.show()
 
+    def mousePressEvent(self, event):
+        self.clickPosition = event.globalPos()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
--- pythonLedy2/UI/ui_functions.py
+++ pythonLedy2/UI/ui_functions.py
@@ -1,12 +1,36 @@
 
 from main import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import Qt
 
 class FunctionsUI(MainWindow):
-    def name_of_button_clicked(self):
+    def nameOfClickedButton(window):
         print("aaa")
         try:
-            nadawca = self.ui.sender()
+            nadawca = window.sender()
         except Exception as e:
             print(e)
         print(nadawca.text())
         print("szisz")
+
+    def changeSizeOfWindow(window):
+        if window.isMaximized():
+            window.showNormal()
+        else:
+            window.showMaximized()
+
+    def slideLeftMenu(window):
+        width = window.ui.slide_menu_container.width()
+
+        if width == 0:
+            newWidth = 200
+        else:
+            newWidth = 0
+
+        window.animation = QtCore.QPropertyAnimation(window.ui.slide_menu_container, b"maximumWidth")
+        window.animation.setDuration(250)
+        window.animation.setStartValue(width)
+        window.animation.setEndValue(newWidth)
+        window.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        window.animation.start()
+
